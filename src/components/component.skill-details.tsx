8dcb0a4d6b3d3e607a8f6aaac006aa50bd8c22de
shinjuno123 <shinjuno123@naver.com>
2024-09-19 17:29:27 -0700

    Add style for skill summary

--- src/components/component.skill-details.tsx
+++ src/components/component.skill-details.tsx
@@ -3,6 +3,7 @@ export function SkillDetails(details: { imageLink: string, skillName: string, de
 
         <div className="skill-details">
             <h3 className="skill-details__skillname">{details.skillName}</h3>
+            <hr />
             <div className="skill-details__description-box">
                 <img src={details.imageLink} alt={details.skillName} />
                 <p className="skill-details__description" dangerouslySetInnerHTML={{__html: details.description}}></p>
