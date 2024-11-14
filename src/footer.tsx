3a3059d73b640bf6f13f2bb210e3ff033d97b290
junho@bobsweep.com <shinjuno123@naver.com>
2024-11-13 19:36:38 -0800

    Add social links

--- src/footer.tsx
+++ src/footer.tsx
@@ -6,26 +6,28 @@ export default function Footer() {
                     <p className="footer__item">
                         <strong>© 2023-2024</strong>
                         <br/>
-                        <a href="">Junho Shin</a>
+                        <a href="/">Junho Shin</a>
                     </p>
 
                     <p className="footer__item">
                         <strong>Work</strong>
                         <br/>
-                        <a href="/"  rel="me">bObsweep</a>
+                        <a href="https://www.bobsweep.com/"  rel="me">bObsweep</a>
                     </p>
 
                     <p className="footer__item">
                         <strong>Social</strong>
                         <br/>
-                        <a href="/"  rel="me">Github</a>
+                        <a href="https://github.com/shinjuno123"  rel="me">Github</a>
+                        <a href="https://www.linkedin.com/in/junho-shin-18b883234/"  rel="me">LinkedIn</a>
                     </p>
 
-                    <p className="footer__item">
+
+                    {/* <p className="footer__item">
                         <strong>Legal</strong>
                         <br/>
                         <a href="/"  rel="me">Privacy Policy</a>
-                    </p>
+                    </p> */}
     
                 </div>
             </footer>
