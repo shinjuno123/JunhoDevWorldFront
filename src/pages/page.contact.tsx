c9eee868ee0f66bb3b3229f2013883cd0cf5bfd2
junho@bobsweep.com <shinjuno123@naver.com>
2024-11-23 20:57:44 -0800

    Add greeting when user submited the form successfully

--- src/pages/page.contact.tsx
+++ src/pages/page.contact.tsx
@@ -1,28 +1,30 @@
 import { FormEvent, useReducer, useState } from "react";
 
-
 interface UserFormState {
   validated: boolean;
   errorMessage: {
-    email: string,
-    name: string,
-    message: string
-  };
-}
-
-interface UserFormAction {
-  type: "VALIDATED" | 'INVALIDATED';
-  errorMessage:  {
-    email: string,
-    name: string,
+    email: string;
+    name: string;
     message: string;
   };
 }
 
-const reducer = (state: UserFormState, action: UserFormAction): UserFormState => {
-  state.validated = action.type === "VALIDATED"? true: false;
+interface UserFormAction {
+  type: "VALIDATED" | "INVALIDATED";
+  errorMessage: {
+    email: string;
+    name: string;
+    message: string;
+  };
+}
+
+const reducer = (
+  state: UserFormState,
+  action: UserFormAction
+): UserFormState => {
+  state.validated = action.type === "VALIDATED" ? true : false;
   state.errorMessage = action.errorMessage;
-  switch(action.type) {
+  switch (action.type) {
     case "VALIDATED":
       state.validated = true;
       break;
@@ -31,12 +33,15 @@ const reducer = (state: UserFormState, action: UserFormAction): UserFormState =>
       break;
   }
 
-  return {...state};
-}
+  return { ...state };
+};
 
 export default function Contact() {
   const [submitted, setSubmitted] = useState<boolean>(false);
-  const [formState, dispatchForm] = useReducer(reducer, {errorMessage: {email: "", message: "", name: ""},validated: false} as UserFormState);
+  const [formState, dispatchForm] = useReducer(reducer, {
+    errorMessage: { email: "", message: "", name: "" },
+    validated: false,
+  } as UserFormState);
 
   function submitMessage(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -44,8 +49,12 @@ export default function Contact() {
     const email = formData.get("email") as string;
     const name = formData.get("name") as string;
     const message = formData.get("message") as string;
-    const errorMessage = {email: "", name: "", message: ""} as UserFormAction['errorMessage'];
-    let type = "VALIDATED" as UserFormAction['type'];
+    const errorMessage = {
+      email: "",
+      name: "",
+      message: "",
+    } as UserFormAction["errorMessage"];
+    let type = "VALIDATED" as UserFormAction["type"];
 
     const emailRegex = /^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
     const nameLengthLimit = 50;
@@ -53,16 +62,29 @@ export default function Contact() {
     // Check empty string
     if (!email.length) {
       errorMessage.email = "Please enter your email!";
+      type = "INVALIDATED";
+    } else {
+      errorMessage.email = "";
     }
 
     if (!name.length) {
       errorMessage.name = "Please enter your name!";
+      type = "INVALIDATED";
+    } else {
+      errorMessage.name = "";
     }
 
     if (!message.length) {
       errorMessage.message = "Please enter the message!";
+      type = "INVALIDATED";
+    } else {
+      errorMessage.message = "";
     }
 
+    if (type === "INVALIDATED") {
+      dispatchForm({ type: type, errorMessage: errorMessage });
+      return;
+    }
 
     // Check the string with a validation
     if (emailRegex.test(email)) {
@@ -73,26 +95,31 @@ export default function Contact() {
     }
 
     if (name.length <= nameLengthLimit) {
-      errorMessage.name = '';
+      errorMessage.name = "";
     } else {
       errorMessage.name = `The entered name has to be within ${nameLengthLimit} characters!`;
       type = "INVALIDATED";
     }
 
-    dispatchForm({type: type, errorMessage: errorMessage});
+    if (type === "INVALIDATED") {
+      dispatchForm({ type: type, errorMessage: errorMessage });
+      return;
+    }
 
     if (type === "VALIDATED") {
+      // Submit the form to server
+
+      // Set the form was already submitted
       setSubmitted(true);
     } else {
       setSubmitted(false);
     }
-
   }
 
   return (
     <>
       <section className="contact container">
-        <div className="contact__inner">
+        <div className={`contact__inner ${submitted ? "hide" : ""}`}>
           <h1>Contact 👜</h1>
           <h3>
             <p>Please fill up the following and submit it 🚀</p>
@@ -102,26 +129,29 @@ export default function Contact() {
             <div className="field">
               <label htmlFor="email">Email: </label>
               <input name="email" type="text" />
-              <p>{`${formState.validated? '':formState.errorMessage.email}`}</p>
+              <p>{`${formState.validated ? "" : formState.errorMessage.email}`}</p>
             </div>
             <div className="field">
               <label htmlFor="name">Your Name: </label>
               <input name="name" type="text" />
-              <p>{`${formState.validated? '':formState.errorMessage.name}`}</p>
+              <p>{`${formState.validated ? "" : formState.errorMessage.name}`}</p>
             </div>
             <div className="field">
               <label htmlFor="message">Message: </label>
               <textarea name="message"></textarea>
-              <p>{`${formState.validated? '':formState.errorMessage.message}`}</p>
+              <p>{`${formState.validated ? "" : formState.errorMessage.message}`}</p>
             </div>
             <div className="button">
               <button type="submit">Submit</button>
             </div>
           </form>
         </div>
-        <div className={`${submitted? 'show':'hide'}`}>
-          <h1>Thank you so much!</h1>
-          <h3>I will reach out to you as soon as possible.</h3>
+        <div className={`greeting ${submitted ? "" : "hide"}`}>
+          <h1>Thank you so much! 👏👏👏</h1>
+          <h3>I will reach out to you as soon as possible 🙏</h3>
+          <div className="button">
+              <button type="button" onClick={() => setSubmitted(false)}>Retry!</button>
+          </div>
         </div>
       </section>
     </>
