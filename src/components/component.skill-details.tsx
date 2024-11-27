a1804b0c9780f95c2812c67fc4fbff64436ad98f
junho@bobsweep.com <shinjuno123@naver.com>
2024-11-26 21:18:38 -0800

    Optimize mobile screen for skill summary page

--- src/components/component.skill-details.tsx
+++ src/components/component.skill-details.tsx
@@ -1,14 +1,58 @@
-export function SkillDetails(details: { imageLink: string, skillName: string, description: string }) {
-    return <>
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
-        <div className="skill-details">
-            <h3 className="skill-details__skillname">{details.skillName}</h3>
-            <hr />
-            <div className="skill-details__description-box">
-                <img src={details.imageLink} alt={details.skillName} />
-                <p className="skill-details__description" dangerouslySetInnerHTML={{__html: details.description}}></p>
-            </div>
+export type SkillDetailsControl = {
+  scrollToDetails: () => void;
+};
+
+export type Callback = () => void;
+
+const SkillDetails = forwardRef<
+  SkillDetailsControl,
+  { imageLink: string; skillName: string; description: string }
+>((_props, ref) => {
+  const skillDetailsRef = useRef<HTMLDivElement>(null);
+  useImperativeHandle(ref, () => ({
+    /**
+     * Scrolls the element referenced by skillDetailsRef into view smoothly.
+     * This function is intended to be used to bring the skill details section
+     * into the user's viewport.
+     * @returns {void}
+     */
+    scrollToDetails: () => {
+      const toTopElement = document.querySelector('.skill-details .to-top');
+      if (toTopElement) window.scrollTo({ top: Math.round(toTopElement.getBoundingClientRect().top + document.documentElement.scrollTop) - 400, behavior: "smooth" });
+    },
+  }));
+
+  /**
+   * Scrolls the window to the top smoothly. This is intended to be
+   * called when the user is done viewing the skill details section
+   * and wants to return to the top of the page.
+   */
+  function toTop() {
+    const ulElement = document.querySelector('.skills');
+    if (ulElement) window.scrollTo({ top: Math.round(ulElement.getBoundingClientRect().top + document.documentElement.scrollTop) - 100, behavior: "smooth" });
+  }
+
+  return (
+    <>
+      <div className="skill-details" ref={skillDetailsRef}>
+        <h3 className="skill-details__skillname">{_props.skillName}</h3>
+        <hr />
+        <div className="skill-details__description-box">
+          <img src={_props.imageLink} alt={_props.skillName} />
+          <p
+            className="skill-details__description"
+            dangerouslySetInnerHTML={{ __html: _props.description }}
+          ></p>
         </div>
-
+        <button type="button" className="to-top" onClick={toTop}>
+          <i className="material-icons">arrow_forward_ios</i>
+          <span>To the top</span>
+        </button>
+      </div>
     </>
-}
+  );
+});
+
+export default SkillDetails;
--- src/pages/page.skills.tsx
+++ src/pages/page.skills.tsx
@@ -1,17 +1,38 @@
-import { SkillDetails } from "../components/component.skill-details";
+import SkillDetails, {
+  SkillDetailsControl,
+} from "../components/component.skill-details";
 import store from "../app/store";
-import { fetchSkills, Skill, SkillResponse } from "../features/skills/skills.slice";
+import {
+  fetchSkills,
+  Skill,
+  SkillResponse,
+} from "../features/skills/skills.slice";
 import { useAppSelector } from "../app/hooks";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import React from "react";
 import { ClipLoader } from "react-spinners";
+import PreviousPage from "../components/component.previous-page-btn";
+import NextPage from "../components/component.next-page-btn";
 
 export default function Skills() {
-  const { skills, loading } = useAppSelector(state => state.skillManager);
-  const [currentSkill, setCurrentSkill] = useState<Skill>({ name: '', id: 0, proficiency: 0, icon: '', description: '' });
+  const { skills, loading } = useAppSelector((state) => state.skillManager);
+  const skillDetailsRef = useRef<SkillDetailsControl>(null);
+  const [currentSkill, setCurrentSkill] = useState<Skill>({
+    name: "",
+    id: 0,
+    proficiency: 0,
+    icon: "",
+    description: "",
+  });
 
+  /**
+   * @function clickSkill
+   * @description Update the current skill with the one that matches the given id
+   * @param {number} id - The id of the skill to be set as the current one
+   */
   function clickSkill(id: number) {
     setCurrentSkill(skills[id]);
+    skillDetailsRef.current?.scrollToDetails();
   }
 
   useEffect(() => {
@@ -23,46 +44,77 @@ export default function Skills() {
     });
 
     return;
-  }, [])
+  }, []);
 
   return (
     <>
       <section className="about__skill-summary local-page">
-
+        <div
+          style={{
+            display: window.location.href.endsWith("/about/skills")
+              ? "block"
+              : "none",
+          }}
+        >
+          <PreviousPage />
+        </div>
         <h1>Skill Summary</h1>
         <p>Please click each of skills below to see the details</p>
         <div className="skills">
-          <li
-            style={{
-              display: loading === 'pending' ? "block" : "none",
-              padding: '15rem 0',
-              textAlign: "center",
-            }}
-          >
-            <ClipLoader />
-          </li>
-          {Object.entries(skills).reverse().map((skills) => {
+          <ul>
+            <li
+              style={{
+                display: loading === "pending" ? "block" : "none",
+                padding: "15rem 0",
+                textAlign: "center",
+              }}
+            >
+              <ClipLoader />
+            </li>
+            {Object.entries(skills)
+              .reverse()
+              .map((skills) => {
+                return (
+                  <React.Fragment key={skills[0]}>
+                    <li
+                      className={`skill ${currentSkill.id === skills[1].id ? "selected" : ""}`}
+                      onClick={() => clickSkill(skills[1].id)}
+                    >
+                      <div className="skill-name">
+                        <span>{skills[1].name}</span>
+                      </div>
+                      <div className="skill-level">
+                        <div
+                          className="skill-level__gauge"
+                          style={{ width: `${skills[1].proficiency}%` }}
+                        ></div>
+                      </div>
+                      <div className="skill-percent">
+                        <span>{skills[1].proficiency}%</span>
+                      </div>
+                    </li>
+                  </React.Fragment>
+                );
+              })}
+          </ul>
 
-            return <React.Fragment key={skills[0]}>
-              <li className={`skill ${currentSkill.id === skills[1].id? 'selected':''}`} onClick={() => clickSkill(skills[1].id)}>
-                <div className="skill-name">
-                  <span>{skills[1].name}</span>
-                </div>
-                <div className="skill-level">
-                  <div
-                    className="skill-level__gauge"
-                    style={{ width: `${skills[1].proficiency}%` }}
-                  ></div>
-                </div>
-                <div className="skill-percent">
-                  <span>{skills[1].proficiency}%</span>
-                </div>
-              </li>
-            </React.Fragment>
-          })}
-          <SkillDetails imageLink={currentSkill.icon} skillName={currentSkill.name} description={currentSkill.description}></SkillDetails>
+          <SkillDetails
+            ref={skillDetailsRef}
+            imageLink={currentSkill.icon}
+            skillName={currentSkill.name}
+            description={currentSkill.description}
+          ></SkillDetails>
         </div>
 
+        <div
+          style={{
+            display: window.location.href.endsWith("/about/skills")
+              ? "block"
+              : "none",
+          }}
+        >
+          <NextPage />
+        </div>
       </section>
     </>
   );
